7ad8a0ebda68d40aa6380fe939a532f0d1661384
mecozma <mecozma@gmail.com>
2021-01-16 23:32:21 +0000

    Day 29.

--- gobook/01/1.2/echo1.go
+++ gobook/01/1.2/echo1.go
@@ -4,13 +4,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var s, sep string
+	var sep string
 	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
 		sep = " "
+		fmt.Println(strconv.Itoa(i) + sep + os.Args[i])
 	}
-	fmt.Println(s)
 }
